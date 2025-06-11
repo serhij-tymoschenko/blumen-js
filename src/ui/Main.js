a44da5fa005496ce78fdf477c37d9a8a429dc749
tymoschenko <serhij.tymoschenko@gmail.com>
2025-06-11 15:06:07 +0200

    Added dependencies for UI and implemented AppBar and BottomBar

---
+++ src/ui/Main.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import ScreenType from "../data/models/ScreenType";
+import {Alert, Box, Snackbar} from "@mui/material";
+import BottomBar from "./components/navigation/BottomBar";
+import TopAppBar from "./components/navigation/TopAppBar";
+
+const Main = () => {
+    const [screenType, setScreenType] = useState(ScreenType.PREVIEW);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+
+    const handleCloseSnackbar = () => {
+        setOpenSnackbar(false);
+    };
+
+    const onButtonClick = (buttonType) => {
+        setScreenType(buttonType);
+    };
+
+    // const screen = (screenType === ScreenType.SVG_TOOL)
+    //     ? <SvgTool setOpenSnackbar={setOpenSnackbar} setSnackbarMessage={setSnackbarMessage}/>
+    //     : <Preview setOpenSnackbar={setOpenSnackbar} setSnackbarMessage={setSnackbarMessage}/>
+
+    return (
+        <Box sx={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
+            <TopAppBar
+                onButtonClick={onButtonClick}
+            />
+            {/*{screen}*/}
+            <BottomBar/>
+            <Snackbar
+                open={openSnackbar}
+                autoHideDuration={3000}
+                onClose={handleCloseSnackbar}
+            >
+                <Alert onClose={handleCloseSnackbar} severity="error" sx={{width: '100%'}}>
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
+        </Box>
+    );
+}
+
+export default Main;
