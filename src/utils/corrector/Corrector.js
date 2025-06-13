b9f785dc09a951927affbfc518dde97136568c45
tymoschenko <serhij.tymoschenko@gmail.com>
2025-06-13 12:52:11 +0200

    Implemented svg corrector

--- src/utils/corrector/Corrector.js
+++ src/utils/corrector/Corrector.js
@@ -1,4 +1,17 @@
-import {COMMENT_REGEX, DOCTYPE_REGEX, XML_REGEX} from "./CorrectorRegex";
+import {
+    COMMENT_REGEX,
+    DOCTYPE_REGEX, EMPTY_LINE_REGEX,
+    INK_GROUP_REGEX,
+    INK_LABEL_REGEX,
+    SINGLE_DEFS_REGEX,
+    SODI_1_REGEX,
+    SODI_2_REGEX,
+    STYLE_REGEX,
+    SVG_REGEX,
+    XML_REGEX
+} from "./CorrectorRegex";
+import {DEFS_REGEX} from "../combiner/CombinerRegex";
+import {addCss} from "./CssHelper";
 
 export const removeRedundantInfo = (svgSrc) => {
     return svgSrc
@@ -7,3 +20,58 @@ export const removeRedundantInfo = (svgSrc) => {
         .replace(COMMENT_REGEX, "")
 }
 
+const correctInkscape = (svgSrc) => {
+    return svgSrc
+        .replace(SODI_1_REGEX, "")
+        .replace(SODI_2_REGEX, "")
+        .replace(INK_LABEL_REGEX, "")
+        .replace(INK_GROUP_REGEX, "")
+}
+
+const correctDesigner = (svgSrc) => {
+    return svgSrc
+        .replace("<style type=\"text/css\">", "<style>");
+}
+
+const correctBlue = (svgSrc) => {
+    return svgSrc
+        .replace(/#00[Ff][Ff]00/g, "blue")
+}
+
+const removeDefs = (svgSrc) => {
+    return svgSrc
+        .replace(DEFS_REGEX, "")
+}
+
+const correctSvg = (svgSrc) => {
+    return svgSrc
+        .replace(SVG_REGEX, "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 380 600\">")
+}
+
+const removeEmptyLines = (svgSrc) => {
+    return svgSrc.replace(EMPTY_LINE_REGEX, "");
+}
+
+const addClasses = (svgSrc) => {
+    if (!STYLE_REGEX.test(svgSrc)) {
+        let localSvg = svgSrc.replace(SINGLE_DEFS_REGEX, "")
+        localSvg = addCss(localSvg)
+        return localSvg
+    }
+    return svgSrc
+}
+
+const correct = (svgSrc) => {
+    let localSvg = svgSrc;
+
+    localSvg = removeRedundantInfo(localSvg)
+    localSvg = correctSvg(localSvg)
+    localSvg = removeDefs(localSvg)
+    localSvg = correctInkscape(localSvg)
+    localSvg = correctDesigner(localSvg)
+    localSvg = addClasses(localSvg)
+    localSvg = correctBlue(localSvg)
+    localSvg = removeEmptyLines(localSvg)
+
+    return localSvg
+}
---
+++ src/utils/corrector/CssHelper.js
@@ -0,0 +1,40 @@
+import {RGB_REGEX, STYLE_PARAM_REGEX} from "../../RegexConstants";
+import {rgbToHex} from "../ColorHelper";
+
+export const addCss = (svgSrc) => {
+    const classes = {};
+    let clsIndex = 0;
+    let localSvg = svgSrc;
+
+    [...svgSrc.matchAll(STYLE_PARAM_REGEX)].forEach(match => {
+        let style = match[1].replace(RGB_REGEX, (match, r, g, b) => {
+            return rgbToHex(parseInt(r), parseInt(g), parseInt(b));
+        });
+
+        classes[clsIndex++] = style;
+        const clsId = Object.keys(classes).find(key => classes[key] === style);
+        localSvg = localSvg.replace(`style="${match[1]}"`, `class="cls-${clsId}"`);
+    })
+
+    let svg = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 380 600\">"
+    let index = localSvg.indexOf(svg) + svg.length;
+
+    const clss = Object.entries(classes)
+        .map(([k, v]) =>
+            `.cls-${k} { ${v.trim()}${v.endsWith(";") ? '' : ';'} }`
+        )
+        .join("\n");
+
+    const styledSvg =
+        localSvg.slice(0, index)
+        + `
+        <defs>
+            <style>
+                ${clss}
+            </style>
+        </defs>
+        `
+        + localSvg.slice(index);
+
+    return styledSvg
+}
